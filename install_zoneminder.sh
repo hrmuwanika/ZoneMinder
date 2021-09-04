a02b356fc8180a34d687bd20e8c6274b0397df39
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-09-04 14:42:04 +0200

    Update install_zoneminder.sh

--- install_zoneminder.sh
+++ install_zoneminder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##############################################################
-#### Installation of zoneminder on Ubuntu 18.04 with LAMP ####
+#### Installation of zoneminder on Ubuntu 20.04 with LAMP ####
 ##############################################################
 #
 #
@@ -10,7 +10,7 @@ ENABLE_SSL="True"
 # Set the website name
 WEBSITE_NAME="example.com"
 # Provide Email to register ssl certificate
-ADMIN_EMAIL="odoo@example.com"
+ADMIN_EMAIL="vms@example.com"
 ##
 #----------------------------------------------------
 # Disable password authentication
@@ -45,37 +45,10 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y ffmpeg
 
-#--------------------------------------------------
-# Install python and opencv
-#--------------------------------------------------
-sudo apt install -y python3-pip python3-dev python3-setuptools python3-opencv libopencv-dev
-sudo python3 -m pip install --upgrade pip
-sudo -H pip3 install opencv-contrib-python
-
-# Install other dependencies
-sudo apt install -y libboost-all-dev libgtk-3-dev scikit-image
-sudo apt install -y build-essential cmake curl git unzip gcc
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
-sudo apt install -y libmodule-build-perl
-sudo apt install -y make
-sudo apt install -y libcrypt-mysql-perl
-sudo apt install -y libcrypt-eksblowfish-perl
-
-sudo perl -MCPAN -e "install Crypt::MySQL"
-sudo perl -MCPAN -e "install Config::IniFiles"
-sudo perl -MCPAN -e "install Crypt::Eksblowfish::Bcrypt"
-sudo perl -MCPAN -e "install Getopt::Long"
-
-sudo apt install -y libyaml-perl
-sudo perl -MCPAN -e "install Net::WebSocket::Server"
-
-sudo apt install -y libjson-perl
-sudo perl -MCPAN -e "install LWP::Protocol::https"
-
 # Install Apache, MySQL, and PHP
 sudo apt install -y tasksel
 tasksel install lamp-server
+sudo apt install -y software-properties-common
 
 #--------------------------------------------------
 # ZoneMinder repository
@@ -83,9 +56,9 @@ tasksel install lamp-server
 sudo add-apt-repository ppa:iconnor/zoneminder-1.36
 sudo apt update
 sudo apt upgrade
-sudo apt dist-upgrade
 
 sudo apt install -y zoneminder
+sudo systemctl enable zoneminder
 
 # Secure MySQL. Do not activate VALIDATE PASSWORD COMPONENT
 #mysql_secure_installation
@@ -102,12 +75,12 @@ EOF
 sudo systemctl restart mysql
 
 # Create the zoneminder database
-sudo mysql -uroot -p < /usr/share/zoneminder/db/zm_create.sql
+sudo mysql -uroot --password="" < /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
 
 #-----------------------------------------------------------------
 # Create user and set permissions (press Enter after each entry)
 #---------------------------------------------------------------
-mysql -u root -p<<MYSQL_SCRIPT
+mysql -u root --password="" <<MYSQL_SCRIPT
 CREATE USER 'zmuser'@localhost IDENTIFIED WITH mysql_native_password BY 'zmpass';
 GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
@@ -130,6 +103,7 @@ sudo a2enmod headers
 # Enable and start the ZoneMinder service
 sudo systemctl enable zoneminder
 sudo systemctl start zoneminder
+sudo systemctl reload apache2
 
 #----------------------------------------------------------
 # set timezone
