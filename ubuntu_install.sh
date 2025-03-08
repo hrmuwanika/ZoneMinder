83202ba1ec0703f5c648e8153a6a66ad5c015296
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-03-08 23:45:48 +0200

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -28,7 +28,7 @@ timedatectl
 sudo apt install -y apache2 php libapache2-mod-php php-mysql msmtp tzdata gnupg ca-certificates  
 
 # Mariadb dependencies
-sudo apt install curl apt-transport-https software-properties-common lsb-release ca-certificates gnupg2 dirmngr
+sudo apt install -y curl apt-transport-https lsb-release gnupg2 dirmngr
 
 curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
 sudo bash mariadb_repo_setup --mariadb-server-version=11.2
@@ -50,43 +50,46 @@ sudo systemctl enable zoneminder
 sudo systemctl start zoneminder
 
 # Secure MySQL. Do not activate VALIDATE PASSWORD COMPONENT
-#mysql_secure_installation
+# mariadb-secure-installation
 
 # Remove mariadb strict mode by setting sql_mode = NO_ENGINE_SUBSTITUTION
-sudo rm /etc/mysql/mariadb.conf.d/50-server.cnf
-cd /etc/mysql/mariadb.conf.d/
-wget https://raw.githubusercontent.com/hrmuwanika/ZoneMinder/master/50-server.cnf
+sed -i '/\[mysqld\]/a sql_mode = NO_ENGINE_SUBSTITUTION' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # create the zoneminder database
-sudo mysql -uroot --password="" < /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
-sudo mysql -uroot --password="" -e "ALTER USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
-sudo mysql -uroot --password="" -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
-sudo mysql -uroot --password="" -e "FLUSH PRIVILEGES;"
-sudo mysqladmin -uroot --password="" reload 2>/dev/null
+sudo mariadb -uroot --password="" -e "create database zm;"
+sudo mariadb -uroot --password="" -e "CREATE USER zmuser@localhost IDENTIFIED BY 'zmpass';"
+sudo mariadb -uroot --password="" -e "GRANT ALL PRIVILEGES ON zm.* TO zmuser@localhost;"
+sudo mariadb -uroot --password="" -e "FLUSH PRIVILEGES;"
+sudo mariadb -uroot --password="" < /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
 
 # Fix permissions
 chmod 740 /etc/zm/zm.conf
 chown root:www-data /etc/zm/zm.conf
 
 sudo adduser www-data video
-chown -R www-data:www-data /usr/share/zoneminder/
-
-# Setup Apache2
-sudo a2enmod rewrite
+sudo chown --recursive www-data:www-data /usr/share/zoneminder/ 
 sudo a2enconf zoneminder
-sudo systemctl restart apache2
+
+# Now we enable the configurations in Apache2
+sudo a2enmod cgi 
+sudo a2enmod rewrite
+
+sudo a2enmod expires 
+sudo a2enmod headers 
+
+sudo systemctl enable zoneminder
+sudo systemctl start zoneminder
+sudo systemctl status zoneminder.service
+netstat -vnatp | grep apache2
 
 #----------------------------------------------------------
 # set timezone
 #----------------------------------------------------------
-sudo sed -i s/";date.timezone =/date.timezone = Africa\/Kigali"/g /etc/php/8.2/apache2/php.ini
+sudo sed -i s/";date.timezone =/date.timezone = Africa\/Kigali"/g /etc/php/8.3/apache2/php.ini
 
 # Restart the Apache2 service
 sudo systemctl restart apache2
 
-# Enable and start the ZoneMinder service
-sudo systemctl restart zoneminder
-
 #----------------------------------------------------
-read -p "Install complete. Open Zoneminder/Options and set the timezone. Press enter to continue" nothing
+echo "Install complete." 
 #----------------------------------------------------
